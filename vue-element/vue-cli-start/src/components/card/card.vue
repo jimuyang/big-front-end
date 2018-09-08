4224ad69fd44441ace94fd9e8c6d44877565fbfe
muyi-macpro <17621660286@163.com>
2018-09-08 23:26:45 +0800

    init the main container

---
+++ vue-element/vue-cli-start/src/components/card/card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="card">
+        <el-row :gutter="12">
+            <el-col :span="8">
+                <el-card shadow="always">
+                    总是显示
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card shadow="hover">
+                    鼠标悬浮时显示
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card shadow="never">
+                    从不显示
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ vue-element/vue-cli-start/src/components/layout/main.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-container id="main-container">
+        <el-aside width="200px">
+            <aside-menu></aside-menu>
+        </el-aside>
+        <el-container>
+            <el-header>
+                <div>
+                <bread-crumb></bread-crumb>
+                </div>
+            </el-header>
+            <el-main>
+                <card></card>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+import Card from '../../components/card/card.vue';
+import AsideMenu from '../../components/aside-menu/menu.vue';
+import BreadCrumb  from '../../components/bread-crumb/bread-crumb.vue';
+
+export default {
+    data: function() {
+        return {
+
+        };
+    },
+    components: {
+        'card': Card,
+        'aside-menu': AsideMenu,
+        'bread-crumb': BreadCrumb
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+#main-container {
+    height: 500px
+}
+.el-header{
+    background: white;
+    div {
+        margin-top: 20px;
+    }
+}
+
+
+</style>
