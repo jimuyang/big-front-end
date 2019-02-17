54e799054e3519ea49a435812565b54248395bdf
muyi_home <17621660286@163.com>
2019-02-17 18:07:51 +0800

    anydoor start

---
+++ node-learning/anydoor/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
