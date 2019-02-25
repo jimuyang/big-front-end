f4853e3bf9d19cb48a66174f079b7c22d763075c
muyi_home <17621660286@163.com>
2019-02-26 00:37:39 +0800

    压缩文件的能力

--- node-learning/anydoor/src/app.js
+++ node-learning/anydoor/src/app.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 const handlebars = require('handlebars');
+const compress = require('./helper/compress');
 
 // dir.tpl
 const tplPath = path.join(__dirname, './templates/dir.tpl');
@@ -23,7 +24,13 @@ const server = http.createServer((req, resp) => {
         if (fileStats.isFile()) {
             resp.statusCode = 200;
             resp.setHeader('Content-type', mime(filePath));
-            fs.createReadStream(filePath).pipe(resp);
+            // 压缩conf.compress定义的文件类型
+            let rs = fs.createReadStream(filePath);
+            if (filePath.match(conf.compress)) {
+                console.log(filePath);
+                rs = compress(rs, req, resp);
+            }
+            rs.pipe(resp);
         } else if (fileStats.isDirectory()) {
             fs.readdir(filePath, (err, files) => {
                 resp.statusCode = 200;
@@ -32,7 +39,7 @@ const server = http.createServer((req, resp) => {
                 const dir = path.relative(conf.root, filePath);
                 const data = {
                     title: path.basename(filePath),
-                    dir: dir || '',
+                    dir: dir ? '/' + dir : '',
                     files
                 };
                 resp.end(dirTpl(data));
--- node-learning/anydoor/src/config/default.js
+++ node-learning/anydoor/src/config/default.js
@@ -1,6 +1,7 @@
 const conf = {
     root: process.cwd(),
     hostname: 'localhost',
-    port: 9888
+    port: 9888,
+    compress: /\.(html|js|css|md|json)/
 };
 module.exports = conf;
